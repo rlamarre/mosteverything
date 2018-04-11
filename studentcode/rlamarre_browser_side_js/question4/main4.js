7348c95bdeb8534a22be2e4fd3c9bb5673d8d1c8
Rebecca La Marre <rebeccalamarre@Rebeccas-Mac-mini.local>
2018-04-11 14:54:25 -0400

    4 finished

--- studentcode/rlamarre_browser_side_js/question4/main4.js
+++ studentcode/rlamarre_browser_side_js/question4/main4.js
@@ -1,15 +1,20 @@
+// create variable to hold content
+
 var app = document.querySelector('#app');
 var body = document.getElementById('body');
-
+// create variable for win and lose
 var win = false;
 var lose = false;
-
+//create variables for buttons
 var buttonStart = document.getElementById('startClick');
 var buttonWin = document.getElementById('winClick');
+// set Win button to invisible at first
 buttonWin.style.display = "none"
-
+// set Win button to appear in random position
 buttonWin.style.top = (100 + Math.random() * 400) + 'px';
 buttonWin.style.left = (100 + Math.random() * 400) + 'px';
+// create div for start Round text
+var roundStartText = document.createElement('div');
 
 
 // win game
@@ -34,15 +39,13 @@ function loseGame() {
 
 function startRound() {
 
+    // remove start game button
     buttonStart.removeEventListener('click', startRound)
 
-    // say round has started
+    // say round has started in new div to not erase existing div html
 
-    // * if this is included code doesn't work*
-
-    app.innerHTML = "Round has Started";
-    app.removeinnerHTML = "Round has Started";
-
+    roundStartText.innerText = "round has started";
+    app.appendChild(roundStartText);
 
     // play round
 
@@ -53,9 +56,11 @@ function startRound() {
     // click button to win
     buttonWin.addEventListener('click', winGame)
 
-    // click anywhere else and lose * doesn't work *
-    
-    // document.addEventListener('click', loseGame);
+    // click anywhere else and lose  - prevent bubbling
+
+    event.stopPropagation()
+
+    document.addEventListener('click', loseGame);
 
     // set timer 
     setTimeout(loseGame, 1500);
@@ -66,3 +71,4 @@ function startRound() {
 // click button to start round
 
 buttonStart.addEventListener('click', startRound)
+
