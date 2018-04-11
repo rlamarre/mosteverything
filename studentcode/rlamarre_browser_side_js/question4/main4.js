42ccddd9f8d3a37b885bd3e45683c148d95a5085
Rebecca La Marre <rebeccalamarre@Rebeccas-Mac-mini.local>
2018-04-11 13:54:29 -0400

    latest changes

--- studentcode/rlamarre_browser_side_js/question4/main4.js
+++ studentcode/rlamarre_browser_side_js/question4/main4.js
@@ -2,59 +2,65 @@ var app = document.querySelector('#app');
 var body = document.getElementById('body');
 
 var win = false;
-var lose = false; 
+var lose = false;
 
 var buttonStart = document.getElementById('startClick');
-
 var buttonWin = document.getElementById('winClick');
-  
-    buttonWin.style.top = (100 + Math.random() * 400) + 'px';
-    buttonWin.style.left = (100 + Math.random() * 400) + 'px'; 
+buttonWin.style.display = "none"
+
+buttonWin.style.top = (100 + Math.random() * 400) + 'px';
+buttonWin.style.left = (100 + Math.random() * 400) + 'px';
 
 
-    // win game
+// win game
 
-function winGame (){
+function winGame() {
     if (!lose) {
         app.innerHTML = "You Win!";
     }
     win = true;
 }
 
-    // lose game
+// lose game
 
-    function loseGame (){
-        if (!win) {
-            app.innerHTML = "You Lose!";
-        }
-        win = true;
+function loseGame() {
+    if (!win) {
+        app.innerHTML = "You Lose!";
     }
+    win = true;
+}
 
-    // start Round
+// start Round
 
-    function startRound (){
-      
-        console.log("this works")
-      // say round has started
-        app.innerHTML= "Round has Started";
+function startRound() {
 
+    buttonStart.removeEventListener('click', startRound)
+
+    // say round has started
+
+    // * if this is included code doesn't work*
+
+    // app.innerHTML = "Round has Started";
+
+    // play round
+
+    // remove start button
+    buttonStart.style.display = "none";
+    // show win button
+    buttonWin.style.display = "block";
     // click button to win
+    buttonWin.addEventListener('click', winGame)
 
-        buttonWin.addEventListener('click', () => {
-            winGame();
-        })
-    // click anywhere else and lose
+    // click anywhere else and lose * doesn't work *
+    
+    // document.addEventListener('click', loseGame);
 
-        body.addEventListener('click', () => {
-            winGame();
-        })       
-    // lose after 1.5 s
-        setTimeout(loseGame, 5000);
+    // set timer on button click
+    document.addEventListener("click", setTimeout((loseGame), 1500));
 
-  };
+}
 
-  // click button to start round
 
-buttonStart.addEventListener('click', () => {
-    setTimeout(startRound, Math.random() * 2000 + 1000);
-})
+// click button to start round
+
+buttonStart.addEventListener('click', startRound)
