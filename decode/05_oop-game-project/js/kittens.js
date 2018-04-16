1d900757051d58011dc776cc85b8e7b7a6017c56
Rebecca La Marre <rebeccalamarre@Rebeccas-Mac-mini.local>
2018-04-16 15:34:27 -0400

    newest changes

--- decode/05_oop-game-project/js/kittens.js
+++ decode/05_oop-game-project/js/kittens.js
@@ -1,7 +1,27 @@
 //var audio = document.createElement('audio');
+
+var gameAudio = new Audio('audio/game_music.mp3');
 function sound() {
-    var audio = new Audio('audio/game_music.mp3');
-    audio.play();
+    gameAudio.play();
+}
+
+var yum = new Audio('audio/yum.mp3');
+function soundEffectBonus() {
+    yum.play();
+    yum.loop = false;
+
+}
+
+
+var gasp = new Audio('audio/gasp.mp3');
+function soundEffectDeath() {
+    gasp.play();
+}
+
+
+
+function soundStop() {
+    gameAudio.pause();
 }
 
 
@@ -11,10 +31,10 @@ var GAME_HEIGHT = 600;
 
 var ENEMY_WIDTH = 75;
 var ENEMY_HEIGHT = 50;
-var MAX_ENEMIES = 3;
+var MAX_ENEMIES = 4;
 
 var BONUS_WIDTH = 75;
-var BONUS_HEIGHT = 75;
+var BONUS_HEIGHT = 50;
 var MAX_BONUS = 1;
 
 var PLAYER_WIDTH = 75;
@@ -34,7 +54,7 @@ var MOVE_DOWN = 'down';
 
 // Preload game images
 var images = {};
-['enemy.png', 'stars.png', 'player.png', 'bonus.png'].forEach(imgName => {
+['enemy.png', 'stars.png', 'player.png', 'bonus.png', 'button.png', 'snowflake.png', 'car.png'].forEach(imgName => {
     var img = document.createElement('img');
     img.src = 'images/' + imgName;
     images[imgName] = img;
@@ -58,7 +78,7 @@ class Enemy extends Entity {
         this.sprite = images['enemy.png'];
 
         // Each enemy should have a different speed
-        this.speed = Math.random() / 1 ;
+        this.speed = Math.random() / 1;
     }
 
     update(timeDiff) {
@@ -70,19 +90,41 @@ class Enemy extends Entity {
     }
 }
 
+class Snowflake extends Entity {
+    constructor(xPos) {
+        super()
+        this.x = xPos;
+        this.y = -ENEMY_HEIGHT;
+        this.sprite = images['snowflake.png'];
+    }
+}
+
 class Bonus extends Entity {
     constructor(xPos) {
         super()
         this.x = xPos;
         this.y = -BONUS_HEIGHT;
         this.sprite = images['bonus.png']
-
+        this.isTouched = false
         this.speed = Math.random() / 2 + 0.25;
+    }
+
+    touched() {
+        this.isTouched = true
     }
 
     update(timeDiff) {
         this.y = this.y + timeDiff * this.speed;
     }
+
+    doWeGetPoints() {
+        if (this.isTouched === false) {
+            console.log('FFF')
+            return true
+        }
+    }
+
+
 
     render(ctx) {
         ctx.drawImage(this.sprite, this.x, this.y);
@@ -140,6 +182,7 @@ class Engine {
 
         // Setup the <canvas> element where we will be drawing
         var canvas = document.createElement('canvas');
+        canvas.id = 'canvas';
         canvas.width = GAME_WIDTH;
         canvas.height = GAME_HEIGHT;
         element.appendChild(canvas);
@@ -254,6 +297,32 @@ class Engine {
         this.player.render(this.ctx); // draw the player
         this.bonuses.forEach(bonus => bonus.render(this.ctx))
 
+
+        // Text for bonus win points
+
+        if (this.bonuses.some((element) => {
+            return (element.isTouched === true)
+        }) === true) {
+            soundEffectBonus()
+            this.ctx.font = 'bold 60px Impact';
+            this.ctx.fillStyle = '#FFCC00';
+            this.ctx.fillText('YUM', 250, 60);
+            this.ctx.fillText('+250', 230, 120);
+        }
+
+        // record of Poutine Consumption
+
+        // function makePoutine(){
+        //     this.ctx.drawImage(images['bonus.png'],0,0)
+        // }
+
+        // if(this.bonuses.some((element) => {
+        //     return (element.isTouched === true)
+        // })=== true){
+        //     for(var i = 1; i <= 4; i++){
+        //         makePoutine()
+        //     }
+
         // Check if any enemies should die
         this.enemies.forEach((enemy, enemyIdx) => {
             if (enemy.y > GAME_HEIGHT) {
@@ -262,10 +331,11 @@ class Engine {
         });
         this.setupEnemies();
 
+
         // Check if any bonuses should disappear
 
         this.bonuses.forEach((bonus, bonusIdx) => {
-            if (bonus.y > GAME_HEIGHT) {
+            if (bonus.y > GAME_HEIGHT + 500) {
                 delete this.bonuses[bonusIdx];
             }
         });
@@ -278,24 +348,54 @@ class Engine {
         // Check if player is dead
         if (this.isPlayerDead()) {
             // If they are dead, then it's game over!
+            soundEffectDeath()
+            soundStop()
             this.ctx.font = 'bold 30px Impact';
-            this.ctx.fillStyle = '#ffffff';
+            this.ctx.fillStyle = '#FF0000';
             this.ctx.fillText(this.score + ' GAME OVER', 5, 30);
+            this.ctx.drawImage(images['button.png'], -55, 0)
+
+            var canvas = document.getElementById('canvas');
+            var rect = {
+                x: -55,
+                y: 0,
+                width: 500,
+                height: 500
+            };
+            function getMousePos(canvas, event) {
+                var rect = canvas.getBoundingClientRect();
+                return {
+                    x: event.clientX - rect.left,
+                    y: event.clientY - rect.top
+                };
+            }
+            function isInside(pos, rect) {
+                return pos.x > rect.x && pos.x < rect.x + rect.width && pos.y < rect.y + rect.height && pos.y > rect.y
+            }
+            canvas.addEventListener('click', function (evt) {
+                var mousePos = getMousePos(canvas, evt);
+                if (isInside(mousePos, rect)) {
+                    location.reload();;
+                }
+            }, false);
         }
+
+
+        // document.addEventListener('click', () => location.reload(true));        }
 
         else {
 
             if (this.isPlayerEating()) {
-                // If they are eating they get a bonus!
-                this.ctx.font = 'bold 30px Impact';
-                this.ctx.fillStyle = '#FFCC00';
-                this.ctx.fillText(this.score + ' YUM *** + 250 pts + ***', 5, 30);
-                console.log("first score :", this.score);
-                this.score += 250;
-                console.log("second score :", this.score);
+
+                // // If they are eating they get a bonus!
+                // this.ctx.font = 'bold 30px Impact';
+                // this.ctx.fillStyle = '#FFCC00';
+                // this.ctx.fillText(this.score + ' YUM *** + 250 pts + ***', 15, 30);
+                //console.log("first score :", this.score);
+                // this.score += 250;
+                //console.log("second score :", this.score);
 
             }
-
             // If player is not dead, then draw the score
             this.ctx.font = 'bold 30px Impact';
             this.ctx.fillStyle = '#ffffff';
@@ -310,6 +410,17 @@ class Engine {
 
     }
 
+    // reStart () {
+    //     var reStartButton = document.createElement('button');
+    //     reStartButton.innerText = 'Play';
+    //     reStartButton.id = 'playAgain';
+    //     reStartButton.style.width = '200px';
+    //     reStartButton.style.height = '100px';
+    //     app.appendChild(reStartButton);
+
+    //     reStartButton.addEventListener('click', () => location.reload(true));
+    // }
+
     isPlayerEating() {
 
         var self = this
@@ -318,24 +429,30 @@ class Engine {
             var bonusAndPlayerInSameColumn = bonus.x === self.player.x
             var bonusReachedPlayer = (bonus.y + BONUS_HEIGHT) >= self.player.y
             var bonusIsPassedPlayer = bonus.y > (self.player.y + PLAYER_HEIGHT)
+            if (bonusAndPlayerInSameColumn && bonusReachedPlayer && !bonusIsPassedPlayer) {
+                if (bonus.doWeGetPoints() === true) {
+                    self.score = self.score + 250
+                }
+                bonus.touched()
+            }
             return bonusAndPlayerInSameColumn && bonusReachedPlayer && !bonusIsPassedPlayer
         })
     }
 
     isPlayerDead() {
-
         var self = this
-
         return this.enemies.some(function (enemy) {
             var enemyAndPlayerInSameColumn = enemy.x === self.player.x
             var enemyReachedPlayer = (enemy.y + ENEMY_HEIGHT) >= self.player.y
             var enemyIsPassedPlayer = enemy.y > (self.player.y + PLAYER_HEIGHT)
-            // var removeMusic =  () => remove sound;
-
             return enemyAndPlayerInSameColumn && enemyReachedPlayer && !enemyIsPassedPlayer;
         })
     }
+
 }
+
+
+
 
 // This section will start the game
 var gameEngine = new Engine(document.getElementById('app'));
